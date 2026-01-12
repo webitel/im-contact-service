91431a8309382517a84fea003b98c01ec3a3fab9
suifri <illia.khvehschuk.pz.2021@lpnu.ua>
2026-01-12 14:44:30 +0200

    [WMSG-138]refactor(contact): add extended unique - change unique constraint to (domain_id, issuer, subject)

--- migrations/20251222092502_create_table_im_contact.sql
+++ migrations/20251222092502_create_table_im_contact.sql
@@ -5,21 +5,17 @@ create table im_contact.contact (
     "domain_id" bigint not null,
     "created_at" timestamptz default now() not null,
     "updated_at" timestamptz default now() not null,
-
     "issuer_id" text not null,
     "application_id" text not null,
     "subject_id" text not null,
-
     "type" text not null,
     "name" text not null default '',
     "username" text not null,
     "metadata" jsonb,
-
     constraint contact_username_not_empty check (trim(username) <> ''),
-    constraint contact_issuer_subject_unique unique(issuer_id, subject_id)
+    constraint contact_issuer_subject_unique unique(domain_id, issuer_id, subject_id)
 );
 -- +goose StatementEnd
-
 -- +goose Down
 -- +goose StatementBegin
 drop table im_contact.contact;
